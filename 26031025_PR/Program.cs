1b45e70d0c3fd9ee7f4423940e7e39aaebd3a3ea
juholim07 <juholim07@gmail.com>
2026-08-26 12:58:00 +0900

    코드 작성

--- 26031025_PR/Program.cs
+++ 26031025_PR/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Hello, World!");
+            Console.SetCursorPosition(5 , 5);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("이름 : 임주호");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
